c1c2be546e5efbe8f2d6ff7d3401792b6a3d3ff1
Franco Maieli <maieli.franco@gmail.com>
2026-06-01 20:36:55 -0300

    TP-Entrega-2.1: se agrega NokiaFlashlight y una clase padre de la cual heredara el otro metodo que haya de iluminacion

---
+++ TGC.MonoGame.TP/SourceCode/Components/Primitives/LightSource.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace TGC.MonoGame.TP.SourceCode.Components
+{
+    internal abstract class LightSource
+    {
+        // Propiedades de la luz
+        public float MaxDurability { get; protected set; }
+        public float Durability { get; protected set; }
+        public float DecayRate { get; protected set; }      // Durabilidad que va perdiendo por segundo
+        public float LightIntensity { get; protected set; } // Intensidad de la luz
+
+        public bool IsActive { get; protected set; }
+
+        protected Model Model;
+        protected Effect Effect;
+
+        public virtual void Toggle()
+        {
+            // Solo se puede prender si le queda bateria
+            if (Durability > 0)
+            {
+                IsActive = !IsActive;
+            }
+            else
+            {
+                IsActive = false;
+            }
+        }
+
+        public virtual void Update(float deltaTime)
+        {
+            if (IsActive)
+            {
+                // Resto durabilidad con el paso del tiempo
+                Durability -= DecayRate * deltaTime;
+
+                if (Durability <= 0)
+                {
+                    Durability = 0;
+                    IsActive = false; // Se apaga sola cuando se agota
+                }
+            }
+        }
+
+        // Los modelos deben de implementar estos metodos para poder funcionar
+        public abstract void LoadContent(ContentManager content, Effect baseEffect);
+        public abstract void Draw(Matrix view, Matrix projection, Matrix cameraWorld);
+    }
+}
